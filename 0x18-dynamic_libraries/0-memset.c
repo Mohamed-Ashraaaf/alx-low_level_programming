194ebc1c56a2d167a00a085b87a843f446a4e7fa
Mohamed-Ashraaaf <mohamed.ashraaaf@gmail.com>
2023-03-24 14:38:29 -0700

    try

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte n amount of times
+ * @s: pointer that has the address of the memory to fill
+ * @c: constant byte that will be written into the memory
+ * @n: amount of times the byte will be written into the memory
+ * Return: A pointer to the filled memory area @s
+ */
+
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}
